4a9ffe83a94bb2c1d83327348ed2f0b59b0aebc5
tbohne <tbohne@uni-osnabrueck.de>
2022-01-23 10:21:25 +0000

    implement plan deployment failure simulations

--- plan_generation/scripts/plan_generator.py
+++ plan_generation/scripts/plan_generator.py
@@ -8,6 +8,33 @@ class PlanGenerator():
 
     def __init__(self):
         self.generate_plan()
+        self.plan_service = None
+
+        rospy.Subscriber('/sim_extended_idle_time', String, self.sim_idle_time_callback, queue_size=1)
+        rospy.Subscriber('/toggle_unavailable_plan_service', String, self.toggle_unavailable_service_callback, queue_size=1)
+        rospy.Subscriber('/sim_empty_plan', String, self.sim_empty_plan_callback, queue_size=1)
+        rospy.Subscriber('/sim_infeasible_plan', String, self.sim_infeasible_plan_callback, queue_size=1)
+        self.sim_extended_idle_time = False
+        self.sim_empty_plan = False
+        self.sim_infeasible_plan = False
+        self.service_available = True
+
+    def sim_idle_time_callback(self, msg):
+        self.sim_extended_idle_time = True
+
+    def sim_empty_plan_callback(self, msg):
+        self.sim_empty_plan = True
+
+    def sim_infeasible_plan_callback(self, msg):
+        self.sim_infeasible_plan = True
+
+    def toggle_unavailable_service_callback(self):
+        if self.service_available:
+            if self.plan_service is not None:
+                self.plan_service.shutdown()
+        else:
+            self.provide_service()
+        self.service_available = not self.service_available
 
     def generate_plan(self):
         self.generated_plan = plan()
@@ -30,10 +57,24 @@ class PlanGenerator():
                 self.generated_plan.actions.append(a)
 
     def retrieve_plan(self, req):
+        if self.sim_extended_idle_time:
+            self.sim_extended_idle_time = False
+            return None
         res = get_planResponse()
         res.generated_plan = self.generated_plan
         res.succeeded = len(self.generated_plan.actions) > 0
+        if self.sim_empty_plan:
+            res.generated_plan.actions = []
+            self.sim_empty_plan = False
+        elif self.sim_infeasible_plan:
+            if len(res.generated_plan.actions) > 0:
+                res.generated_plan.actions[0].name = "unknown"
+            self.sim_infeasible_plan = False
         return res
+
+    def provide_service(self):
+        rospy.loginfo("providing plan generation service..")
+        self.plan_service = rospy.Service('arox_planner/get_plan', get_plan, self.retrieve_plan)
 
 def node():
     rospy.init_node('plan_generator')
@@ -43,8 +84,7 @@ def node():
     rospy.loginfo("setting handcrafted plan..")
     plan_generator.generate_plan()
 
-    rospy.loginfo("providing plan generation service..")
-    service = rospy.Service('arox_planner/get_plan', get_plan, plan_generator.retrieve_plan)
+    plan_generator.provide_service()
 
     rospy.spin()
 
